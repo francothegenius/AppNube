0c2c3a2edf4f65e4a6216c55bc1299441eda113c
Dalia <a01635883@itesm.mx>
2021-09-29 22:16:58 -0500

    2

--- src/test.js
+++ src/test.js
@@ -1,18 +1,28 @@
 let miTitulo,miClave,miTexto;
+const Http = new XMLHttpRequest();
+const url='https://raw.githubusercontent.com/francothegenius/AppNube/htmlBranch/src/rut/ele.json';
+Http.open("GET", url);
+Http.send();
 
+
+    const requestURL = 'https://raw.githubusercontent.com/francothegenius/AppNube/htmlBranch/src/rut/ele.json';
+    const request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    
 function escribe() {
     escribir = document.getElementById("caja")
-    miTitulo = "<h1>" + document.rellenar.titulo.value + "</h1>"
-    miClave = "<h3>" + document.rellenar.clave.value + "</h3>"
-    miTexto = "<p>" + document.rellenar.texto.value + "</p>"
-    escribir.innerHTML = miTitulo + miClave + miTexto
+    //miTitulo = "<h1>" + document.rellenar.titulo.value + "</h1>"
+    const superHeroes = request.response;
+    escribir.innerHTML = superHeroes["result"][1]["Direccion"];
     }
-/*function delt() {
+function delt() {
     escribir = document.getElementById("caja")
-    escribir.innerHTML = ""
-}*/
-document.getElementById("demo").innerHTML = "Hello JavaScript!";
+    escribir.innerHTML = Http.responseText
+}/**/
+
 window.onload = function() {
     document.rellenar.ver.onclick = escribe
-    //document.rellenar2.del.onclick=delt
+    document.rellenar2.del.onclick=delt
 }
